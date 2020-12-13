195b64e2ce7f62a5263d69f294b49c6079315990
Tasfia Islam <tasf1a027@gmail.com>
2020-12-13 12:49:09 +0600

    Minor changes

--- cvproject/cvapp/views.py
+++ cvproject/cvapp/views.py
@@ -46,65 +46,69 @@ def LoginView(request):
 
 
 def UserDataView(request):
+
+    token = request.session['api_token']
+    # on_spot_created_time = int(round(time.time() * 1000))
+    # on_spot_updated_time = int(round(time.time() * 1000))
+    # tsync_id = str(uuid.uuid4())
+    # cv_tsync_id = str(uuid.uuid4())
     form = UserDataForm()
 
     if request.method == 'POST':
         form = UserDataForm(request.POST, request.FILES)
         if form.is_valid():
 
-            token = request.session['api_token']
-
-            on_spot_created_time = int(round(time.time() * 1000))
-            on_spot_updated_time = int(round(time.time() * 1000))
-            tsync_id = str(uuid.uuid4())
-            cv_tsync_id = str(uuid.uuid4())
-
-            file = request.FILES['cv_file']
-            files = {'file': file.read()}
+            cv_get_file = request.FILES['cv_file']
+            files = {'file': cv_get_file.read()}
 
             # json_data = json.loads(request.body)
-            headers = {'content-type': 'application/json',
+            headers = {'Content-Type': 'application/json',
                        'Authorization': f'Token {token}'}
 
-            userData = {"tsync_id": tsync_id,
-                        "name": form.data['name'],
-                        "email": form.data['email'],
-                        "phone": form.data['phone'],
-                        "full_address": form.data['full_address'],
-                        "name_of_university": form.data['name_of_university'],
-                        "graduation_year": form.data['graduation_year'],
-                        "cgpa": form.data['cgpa'],
-                        "experience_in_months": form.data['experience_in_months'],
-                        "current_work_place_name": form.data['current_work_place_name'],
-                        "applying_in": form.data['applying_in'],
-                        "expected_salary": form.data['expected_salary'],
-                        "field_buzz_reference": form.data['field_buzz_reference'],
-                        "github_project_url": form.data['github_project_url'],
-                        "cv_file": {
-                            "tsync_Id": cv_tsync_id},
-                        "on_spot_created_time": on_spot_created_time,
-                        "on_spot_updated_time": on_spot_updated_time,
+            userData = {'tsync_id': tsync_id,
+                        'name': request.POST.get('name'),
+                        'email': request.POST.get('email'),
+                        'phone': request.POST.get('phone'),
+                        'full_address': request.POST.get('full_address'),
+                        'name_of_university': request.POST.get('name_of_university'),
+                        'graduation_year': request.POST.get('graduation_year'),
+                        'cgpa': request.POST.get('cgpa'),
+                        'experience_in_months': request.POST.get('experience_in_months'),
+                        'current_work_place_name': request.POST.get('current_work_place_name'),
+                        'applying_in': request.POST.get('applying_in'),
+                        'expected_salary': request.POST.get('expected_salary'),
+                        'field_buzz_reference': request.POST.get('field_buzz_reference'),
+                        'github_project_url': request.POST.get('github_project_url'),
+                        'cv_file': {
+                            'tsync_Id': cv_tsync_id},
+                        'on_spot_created_time': on_spot_created_time,
+                        'on_spot_updated_time': on_spot_updated_time,
                         }
 
-            r = requests.post(
-                "https://recruitment.fisdev.com/api/v1/recruiting-entities/", data=userData, headers=headers)
+            cv_response = requests.post(
+                'https://recruitment.fisdev.com/api/v0/recruiting-entities/', data=userData, headers=headers)
 
-            if r.status_code == 200:
+            data_response = cv_response.json()
+
+            if cv_response.status_code == 200:
+                messages.success(request, 'Form submission successful')
                 response = r.json()
 
-                FILE_TOKEN_ID = response['cv_file']['id']
-                headers = {'content-type': 'application/pdf',
-                           'Authorization': f'Token {token}'}
+                FILE_TOKEN_ID = data_response['cv_file']['id']
+
+                headers = {
+                    'Authorization': f'Token {token}'}
+
                 file_url = "https://recruitment.fisdev.com/api/file-object/{FILE_TOKEN_ID}/"
 
-                file = request.FILES['cv_file']
-                files = {'file': file.read()}
+                get_file = request.FILES['cv_file']
+                files = {'file': get_file.read()}
 
-                r = requests.post(
+                file_request = request.put(
                     file_url, files=files, headers=headers)
 
-                if r.status_code == 200:
-                    messages.success(request, 'Form submission successful')
+                if file_request.status_code == 200:
+                    messages.success(request, 'File submission successful')
                     return redirect('home')
 
     context = {'form': form}
